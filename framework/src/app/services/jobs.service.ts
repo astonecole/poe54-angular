2b5e61234d142ebf64d1eafd57886d9839191ebf
Vincent Letourneau <vincent@nanoninja.com>
2019-07-26 15:48:52 +0200

    Improve Service

--- framework/src/app/services/jobs.service.ts
+++ framework/src/app/services/jobs.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Job } from '../components/jobs/models/job.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,21 @@ export class JobsService {
 
   add(job: Job): Observable<Job> {
     return this.http.post<Job>('/jobs', job);
+  }
+
+  all(): Observable<Job[]> {
+    return this.http.get('/jobs')
+      .pipe(
+        map(
+          (jobs: Job[]) => jobs)
+      );
+  }
+
+  update(job: Job): Observable<Job> {
+    return this.http.put<Job>('/jobs', job);
+  }
+
+  delete(job: Job): Observable<any> {
+    return this.http.delete('/jobs/' + job.id);
   }
 }
